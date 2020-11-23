4ac9968be9830d9faea2e78311e31c4a23713fb6
AlvaroBarrosJunior <alvarojoaquimjr@gmail.com>
2020-11-23 14:49:34 -0300

    iniciei a criação dos componentes visuais

---
+++ pokedex/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PokeCard from './components/PokeCard/PokeCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PokeCard />
+    </div>
+  );
+}
+
+export default App;
