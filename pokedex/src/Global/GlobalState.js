93bb22c99724bcf83e5e28c1186ef6760d8d8156
Diegodossantos23 <diegodossantos900@outlook.com>
2020-11-26 11:11:18 -0300

    Files Update

---
+++ pokedex/src/Global/GlobalState.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import GlobalStateContext from './GlobalStateContext'
+
+const GlobalState = (props) => {
+  const [pokemonList, setPokemonList] = useState([])
+  const [pokedex, setPokedex] = useState([])
+
+  const getPokemonList = () =>{ 
+    axios.get("https://pokeapi.co/api/v2/pokemon").then(response => {
+        setPokemonList(response.data.results)
+    })
+    .catch(error =>{console.log(error)})
+  }
+
+  const states = {pokemonList, pokedex}
+  const setters = {setPokemonList, setPokedex}
+  const requests = {getPokemonList}
+
+  const data = {states, setters, requests}
+
+  return (
+    <GlobalStateContext.Provider value={data}>
+      {props.children}
+    </GlobalStateContext.Provider>
+  )
+}
+
+export default GlobalState;
--- pokedex/src/screens/HomePage/index.js
+++ pokedex/src/screens/HomePage/index.js
@@ -1,39 +1,51 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import PokeCard from '../../components/PokeCard/PokeCard';
 import PokeDexButton from '../../components/Buttons/PokeDexButton/index'
 import {Header} from '../../components/Header/index'
-
 import {HomeFlexBox} from '../../components/Main/index'
-import GetHooks from '../../components/Hooks/index';
+import GlobalStateContext from '../../Global/GlobalStateContext';
 
 
-const HomePage = () => {
+  const HomePage = () => {
+  const {states, setters, requests} = useContext(GlobalStateContext)
+
+  useEffect(()=>{
+    requests.getPokemonList()
+  }, [])
+
+  const addToPokedex = (newPokemon) => {
+    console.log("entrei")
+    const pokemonInPokedex = states.pokedex.find((pokemon) => newPokemon.name === pokemon.name)
+    if (!pokemonInPokedex) {
+      const pokemonToAdd = states.pokemonList.find((pokemon) => newPokemon.name === pokemon.name)
+      const newPokedex = [...states.pokedex, pokemonToAdd]
+      setters.setPokedex(newPokedex)
+    }
+  }
+
+  console.log(states.pokedex)
+
+  // const renderedPokemons = () => {
+  //   pokemonList.map((pokemon) => {
+  //     return (<PokeCard key={pokemon.name} photo={photo} name={pokemon.name}/>)
+  //   })
+  // }
+
   return(
     <div>
       <Header>
-        <PokeDexButton/>
-          <h1>Lista de pokemons</h1>         
+          <PokeDexButton/>
+          <h1>Lista de Pokemons</h1>
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/770px-Pok%C3%A9_Ball_icon.svg.png" width="50px" />
       </Header>
 
       <HomeFlexBox>
-          <GetHooks></GetHooks>
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
-          <PokeCard />
+          
+        {states.pokemonList && 
+          states.pokemonList.map((pokemon) => {
+            return (<PokeCard key={pokemon.name} pokemon={pokemon} addToPokedex={() => addToPokedex(pokemon)}/>)
+          })
+        }
       </HomeFlexBox>
     </div>
   )
