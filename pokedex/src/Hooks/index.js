776b174257fb48c15e51da7de84078237eaafc7e
Diegodossantos23 <diegodossantos900@outlook.com>
2020-11-27 01:32:08 -0300

    subindo pra nuvem

--- pokedex/src/Hooks/index.js
+++ pokedex/src/Hooks/index.js
@@ -1,6 +1,6 @@
 import  React, { useEffect, useState } from 'react'
 import axios from 'axios'
-
+//teste
 const GetHooks = () => {
     const [dados,setDados] = useState([])
     const getDados = () =>{ 
